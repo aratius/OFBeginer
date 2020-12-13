190bd91d151d4943420c73bd077848070fb5cb61
aratius <arata1129matsu@icloud.com>
2020-12-13 17:35:19 +0900

    [3]たくさんのボール。クリックでマウスに向かってイージングしながら集まってくる

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,42 +2,72 @@
 
 #define BALL_NUM 1000
 
-float loc_x;
-float loc_y;
+float loc_x[BALL_NUM];
+float loc_y[BALL_NUM];
+float speed_x[BALL_NUM];
+float speed_y[BALL_NUM];
+float radius[BALL_NUM];
+float red[BALL_NUM];
+float green[BALL_NUM];
+float blue[BALL_NUM];
+bool mouse_pressed;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofBackground(0, 0, 0);
     ofSetFrameRate(60);
     ofSetCircleResolution(64);
+    ofEnableAlphaBlending();
     
-    loc_x = 0;
-    loc_y = 0;
+    mouse_pressed = false;
+    
+    for(int i = 0; i < BALL_NUM; i ++) {
+        loc_x[i] = ofRandom(0, ofGetWidth());
+        loc_y[i] = ofRandom(0, ofGetHeight());
+        speed_x[i] = ofRandom(-5, 5);
+        speed_y[i] = ofRandom(-5, 5);
+        radius[i] = ofRandom(4, 40);
+        red[i] = ofRandom(0, 255);
+        green[i] = ofRandom(0, 255);
+        blue[i] = ofRandom(0, 255);
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    loc_x += 3;
-    loc_y += 2;
-    
-    if(loc_x < 0) {
-        loc_x = ofGetWidth();
-    }
-    if(loc_x > ofGetWidth()) {
-        loc_x = 0;
-    }
-    if(loc_y < 0) {
-        loc_y = ofGetHeight();
-    }
-    if(loc_y > ofGetHeight()) {
-        loc_y = 0;
+    for(int i = 0; i < BALL_NUM; i ++) {
+        
+        //if mouse_pressed balls follows to mouse
+        if(mouse_pressed) {
+            speed_x[i] = (mouseX - loc_x[i]) / 8.0;
+            speed_y[i] = (mouseY - loc_y[i]) / 8.0;
+        }
+        
+        //update position
+        loc_x[i] += speed_x[i];
+        loc_y[i] += speed_y[i];
+        
+        if(loc_x[i] < 0) {
+            speed_x[i] *= -1;
+        }
+        if(loc_x[i] > ofGetWidth()) {
+            speed_x[i] *= -1;
+        }
+        if(loc_y[i] < 0) {
+            speed_y[i] *= -1;
+        }
+        if(loc_y[i] > ofGetHeight()) {
+            speed_y[i] *= -1;
+        }
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofSetColor(31, 63, 255);
-    ofCircle(loc_x, loc_y, 40);
+    for(int i = 0; i < BALL_NUM; i ++) {
+        ofSetColor(red[i], green[i], blue[i]);
+        ofCircle(loc_x[i], loc_y[i], radius[i]);
+    }
 }
 
 //--------------------------------------------------------------
@@ -62,12 +92,17 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    mouse_pressed = true;
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    mouse_pressed = false;
+    
+    for(int i = 0; i < BALL_NUM; i ++ ){
+        speed_x[i] = ofRandom(-5, 5);
+        speed_y[i] = ofRandom(-5, 5);
+    }
 }
 
 //--------------------------------------------------------------

2160f809a818c5f139ce5122608d05edf767b450
aratius <arata1129matsu@icloud.com>
2020-12-13 16:13:42 +0900

    [1]配列と定数の使い方

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,18 +1,38 @@
 #include "ofApp.h"
 
+#define BALL_NUM 1000
+
+float x[BALL_NUM];
+float y[BALL_NUM];
+float radius[BALL_NUM];
+
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    int i;
+    ofBackground(0, 0, 0);
+    ofEnableAlphaBlending();
+    ofSetCircleResolution(64);
+    
+    for(i = 0; i < BALL_NUM; i++) {
+        x[i] = ofRandom(0, ofGetWidth());
+        y[i] = ofRandom(0, ofGetHeight());
+        radius[i] = ofRandom(10, 40);
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    int i;
+    ofSetColor(31, 63, 255, 63);
+    
+    for(i = 0; i < BALL_NUM; i++) {
+        ofCircle(x[i], y[i], radius[i]);
+    }
 }
 
 //--------------------------------------------------------------

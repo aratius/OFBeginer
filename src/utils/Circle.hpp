b57497252b355da17c1b80a7c7225cf691ddb119
aratius <arata1129matsu@icloud.com>
2020-12-13 22:23:47 +0900

    releaseでできたファイル群？

---
+++ src/utils/Circle.hpp
@@ -0,0 +1,28 @@
+//
+//  Circle.hpp
+//  beginner
+//
+//  Created by 松本新 on 2020/12/13.
+
+#include "ofMain.h"
+
+#ifndef Circle_hpp
+#define Circle_hpp
+
+#include <stdio.h>
+
+class Circle {
+    
+public:
+    
+    void init(float _y, int _eSize, float _speed);
+    void update();
+    void display();
+    
+    float xPos, yPos, eSize, speed;
+    
+private:
+    
+};
+
+#endif /* Circle_hpp */
